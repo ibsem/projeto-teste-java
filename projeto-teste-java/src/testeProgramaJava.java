f6e7b5d60399370ac932ee810d953def598ae79d
ibsem <ibsem.dias@gmail.com>
2019-08-06 19:46:03 -0300

    teste de commit de versao

--- projeto-teste-java/src/testeProgramaJava.java
+++ projeto-teste-java/src/testeProgramaJava.java
@@ -3,7 +3,8 @@ public class testeProgramaJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Ol· java!");
+		System.out.println("Ol√° java!");
+		System.out.println("teste de commit de versao");
 	}
 
 }
